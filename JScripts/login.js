14236ebeb6ac98a8495aa168d6300b7a49846499
Je <114700521+WolvieDeveloper@users.noreply.github.com>
2022-11-04 17:25:13 +0800

    Add files via upload

---
+++ JScripts/login.js
@@ -0,0 +1,21 @@
+function login(){
+    user = document.getElementById("user").value;
+    pass = document.getElementById("pword").value;
+
+    if( user == "Jerome" && pass == "Marino"){
+        alert("Welcome Master")
+        window.open("THomepage.html");
+    }
+    else{alert("You not master")
+    }
+
+}
+function dumdum(){
+    alert("Password is 'Marino' you dum dum")
+}
+function lol(){
+    alert("This is not real webpage you weirdo")
+}
+function contact(){
+    alert("you may contact me NEVER!!!")
+}
